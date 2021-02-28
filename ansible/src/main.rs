ff6deece00c58d79b762346fe9cdf55963883072
Luiz Irber <luiz.irber@gmail.com>
2021-02-27 17:37:09 -0800

    a fancy version, with a proper tokenizer, stemming and stop words removal

--- ansible/src/main.rs
+++ ansible/src/main.rs
@@ -1,16 +1,19 @@
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 use std::fs::File;
 use std::hash::{BuildHasher, BuildHasherDefault, Hash, Hasher};
 use std::io::{BufRead, BufReader, BufWriter};
+use std::iter::FromIterator;
 use std::path::Path;
 
 use anyhow::Result;
 use camino::Utf8PathBuf as PathBuf;
+use rust_stemmers::{Algorithm, Stemmer};
 use sourmash::index::storage::ToWriter;
 use sourmash::signature::{Signature, SigsTrait};
 use sourmash::sketch::minhash::{max_hash_for_scaled, KmerMinHash};
 use sourmash::sketch::Sketch;
 use structopt::StructOpt;
+use vtext::tokenize::{RegexpTokenizer, Tokenizer};
 
 #[derive(StructOpt, Debug)]
 enum Cli {
@@ -33,11 +36,102 @@ enum Cli {
     },
 }
 
+fn sketch_fancy<P: AsRef<Path>>(dataset: P, template: &Sketch) -> Result<Sketch> {
+    // Init sketch
+    let mut mh = template.clone();
+    let ngram_size = template.ksize();
+
+    // Open dataset as text
+    let file = File::open(dataset)?;
+    let reader = BufReader::new(file);
+
+    let common_words: HashSet<String> = HashSet::from_iter(stop_words::get("english"));
+
+    let s = BuildHasherDefault::<twox_hash::Xxh3Hash128>::default();
+    let mut hasher = s.build_hasher();
+    let mut current_ngram: VecDeque<String> = VecDeque::with_capacity(ngram_size);
+    let tokenizer = RegexpTokenizer::default();
+    let en_stemmer = Stemmer::create(Algorithm::English);
+
+    for line in reader.lines() {
+        let line = line?;
+        // use the vtext tokenizer
+        for word in tokenizer.tokenize(&line) {
+            let word: String = en_stemmer.stem(word).into();
+            //let word: String = word.into();
+
+            if !word.is_empty() && !common_words.contains(&word) {
+                current_ngram.push_back(word.to_lowercase());
+            };
+
+            if current_ngram.len() == ngram_size {
+                current_ngram.hash(&mut hasher);
+                let hash = hasher.finish();
+                hasher = s.build_hasher();
+                current_ngram.pop_front();
+
+                // add to sketch (skip add_word, use add_hash directly)
+                if let Sketch::MinHash(ref mut mh) = mh {
+                    mh.add_hash(hash);
+                };
+            }
+        }
+    }
+
+    Ok(mh)
+}
+
+fn build_template(scaled: usize, ngram: u8) -> Sketch {
+    let max_hash = max_hash_for_scaled(scaled as u64);
+    let template_mh = KmerMinHash::builder()
+        .num(0)
+        .ksize(ngram as u32)
+        .max_hash(max_hash)
+        .build();
+    Sketch::MinHash(template_mh)
+}
+
+fn main() -> Result<()> {
+    match Cli::from_args() {
+        Cli::Sketch {
+            dataset,
+            output,
+            ngram,
+            scaled,
+        } => {
+            let template = build_template(scaled, ngram);
+            let mh = sketch_fancy(&dataset, &template)?;
+
+            // save mh
+            let sig = Signature::builder()
+                .name(Some(dataset.file_name().unwrap().into()))
+                .hash_function("0.xxhash_ngram")
+                .filename(None)
+                .signatures(vec![mh])
+                .build();
+
+            let outpath: PathBuf = if let Some(p) = output {
+                p
+            } else {
+                let mut path: PathBuf = dataset;
+                path.set_extension("sig");
+                path
+            };
+            let mut out = BufWriter::new(File::create(outpath)?);
+            sig.to_writer(&mut out)?;
+        }
+    };
+
+    Ok(())
+}
+
+/* This is the initial try, where I'm tokenizing and trimming punctuation myself
+
 fn trim_punctuation(c: char) -> bool {
     matches!(c, '(' | ')' | '-' | '"' | ',' | '?' | '!' | '“' | '”' | '.')
 }
 
-fn sketch<P: AsRef<Path>>(dataset: P, template: &Sketch) -> Result<Sketch> {
+fn sketch_naive<P: AsRef<Path>>(dataset: P, template: &Sketch) -> Result<Sketch> {
     // Init sketch
     let mut mh = template.clone();
     let ngram_size = template.ksize();
@@ -77,47 +171,4 @@ fn sketch<P: AsRef<Path>>(dataset: P, template: &Sketch) -> Result<Sketch> {
 
     Ok(mh)
 }
-
-fn build_template(scaled: usize, ngram: u8) -> Sketch {
-    let max_hash = max_hash_for_scaled(scaled as u64);
-    let template_mh = KmerMinHash::builder()
-        .num(0)
-        .ksize(ngram as u32)
-        .max_hash(max_hash)
-        .build();
-    Sketch::MinHash(template_mh)
-}
-
-fn main() -> Result<()> {
-    match Cli::from_args() {
-        Cli::Sketch {
-            dataset,
-            output,
-            ngram,
-            scaled,
-        } => {
-            let template = build_template(scaled, ngram);
-            let mh = sketch(&dataset, &template)?;
-
-            // save mh
-            let sig = Signature::builder()
-                .name(Some(dataset.file_name().unwrap().into()))
-                .hash_function("0.xxhash_ngram")
-                .filename(None)
-                .signatures(vec![mh])
-                .build();
-
-            let outpath: PathBuf = if let Some(p) = output {
-                p
-            } else {
-                let mut path: PathBuf = dataset;
-                path.set_extension("sig");
-                path
-            };
-            let mut out = BufWriter::new(File::create(outpath)?);
-            sig.to_writer(&mut out)?;
-        }
-    };
-
-    Ok(())
-}
+*/
